569be311ef34f797b9c1041f8eb24a1840c100e5
sagittaracc <sagittaracc@gmail.com>
2026-03-09 11:12:33 +0300

    update script

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const k = 5;
 
+const make_even = x => k * x + 1;
 const is_even = (x) => x % 2 === 0;
 const is_odd = (x) => !is_even(x);
+const isPowerOfTwo = (x) => x > 0 && (x & (x - 1)) === 0;
 
 const harut = (x) => {
     if (is_odd(x)) {
-        return k * x + 1;
+        return make_even(x);
     } else {
         let m = k - 1;
         while (m >= 2) {
@@ -17,7 +19,7 @@ const harut = (x) => {
 
 const iterationLimit = 1e6;
 
-for (let x0 = 2, cx0 = x0; x0 <= 100000; x0++) {
+for (let x0 = 2, cx0 = x0; x0 <= 1e5; x0++) {
     let iterationCount = 0;
 
     while (true) {
@@ -26,12 +28,14 @@ for (let x0 = 2, cx0 = x0; x0 <= 100000; x0++) {
             process.exit(1);
         }
     
-        if (cx0 === 1) {
+        // if (cx0 === 1) {
+        if (isPowerOfTwo(cx0)) {
             console.log('Reached the number 1 for ', x0);
             break;
         }
     
-        cx0 = harut(cx0);
+        // cx0 = harut(cx0);
+        cx0 = make_even(cx0);
     
         iterationCount++;
     }
